7545a40bdd30d129b3ebfd0e23462498f92c262c
FrancoDodera <francododera@hotmail.com>
2023-08-29 15:20:35 -0300

    Home start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Layout from "./Layout";
+import Home from "./components/Home/Home";
 import LoginPage from "./components/LoginPage/LoginPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
-      <Route path="/" element={<LoginPage />}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/register" element={<RegisterPage />}></Route>
     </Routes>
   );
 }
