9044b1c9af3d768056748d980dca298dbe654a9b
FrancoDodera <francododera@hotmail.com>
2023-08-29 16:13:30 -0300

    avances

--- client/src/components/RegisterPage/RegisterPage.jsx
+++ client/src/components/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Style from "./RegisterPage.module.css";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div className={Style.container}>
+      <form className={Style.register}>
+        <h1 className={Style.h1}>Register</h1>
+        <div className={Style.cont}>
+          <input className={Style.input} type="text" placeholder="Username" />
+          <input className={Style.input} type="text" placeholder="Email" />
+        </div>
+
+        <div className={Style.cont}>
+          <input
+            className={Style.input}
+            type="password"
+            placeholder="Password"
+          />
+          <input
+            className={Style.input}
+            type="password"
+            placeholder="Repeat Passowrd"
+          />
+        </div>
+        <button className={Style.button}>Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterPage;
